49a823968e8bcff50561139db92bd91d8fdab378
gorkavillar <gorka@gorkavillar.com>
2022-11-21 19:52:36 +0100

    Sesión 01 - Introducción, Ejecución y Sintaxis con Typescript

---
+++ src/primer-archivo.ts
@@ -0,0 +1,8 @@
+const num1: number = 2;
+const num2: number = 6;
+
+function suma(a: number, b: number) {
+    return a + b
+}
+
+console.log(`La suma de los dos números en función es ${suma(num1, num2)}`)
