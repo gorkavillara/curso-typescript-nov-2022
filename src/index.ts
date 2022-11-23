270a4390fdb3573af173707b232333c3aafc82c9
gorkavillar <gorka@gorkavillar.com>
2022-11-23 16:42:52 +0100

    Tema 05 - Finalizado

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ const obtenPersonaje = async () => {
     const url: string = "https://swapi.dev/api/people/1"
     const data: AxiosResponse<any, any> = await axios.get(url)
     console.log(data.data)
+    // Comentario
 }
 
 obtenPersonaje()
