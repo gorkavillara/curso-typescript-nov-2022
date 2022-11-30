51c7b4807651d185094e012bbc762be160e2469d
gorkavillar <gorka@gorkavillar.com>
2022-11-30 17:02:39 +0100

    ✏️ Importando y utilizando chalk

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log("Hola desde typescript")
+import chalk from "chalk" // ES6 Modules
+// const chalk = require("chalk") // CommonJS
+
+console.log(chalk.bgBlueBright("Hola desde Typescript"))
+
+// CommonJS => require('chalk')
+// ESmodules => import ...
